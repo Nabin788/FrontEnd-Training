253ab64f494da9a48e9d755b06e9ead068502d18
Nabin Poudel <nabinpoudel788@gmail.com>
2024-07-04 02:35:35 +0545

    function in javaScript

---
+++ javaScript/function.js
@@ -0,0 +1,26 @@
+//! function in javaScript
+
+// function declaration
+function sum(a,b){
+    return a+b;
+}
+// function declaration
+
+// function call
+sum(5,3);
+// function call
+
+
+function greet(name){
+    console.log(`Hello ${name}, welcome to 2024 jsvaScript course`);
+}
+greet("thapa");
+greet("ram");
+
+// * expression function or anonymous function
+var result = function (a,b){
+    console.log(a+b);
+}
+result(100,200);
+// * expression function or anonymous function
+
