dbefb4cee45c0d5361986889cbcc9b994db64d2b
Nabin Poudel <nabinpoudel788@gmail.com>
2024-07-04 21:48:36 +0545

    array in javaScript

---
+++ javaScript/array.js
@@ -0,0 +1,17 @@
+// ! Array is an object. in java script every thing id sn object. array start with 0. basically first is always 0 instead of 1.
+
+// * array litera
+const allName = ['nabin', 'siwan', 'sabina', 'mom', 'father', 'nabina']
+console.log(allName[5]);
+console.log(allName);
+
+// ? Array using constructor
+const fruit = new Array('apple', 'banana', 'orange', 'mango')
+console.log(fruit[3]);
+console.log(fruit);
+
+// *we can also modify the element of an array 
+fruit[3] = "stroibery";
+console.log(fruit);
+
+
