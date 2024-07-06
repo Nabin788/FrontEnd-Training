e7fb1c72bbdabed7570ad00957e769821269f259
Nabin Poudel <nabinpoudel788@gmail.com>
2024-07-06 18:20:58 +0545

    no changes

---
+++ javaScript/var.js
@@ -0,0 +1,37 @@
+// valid variable declaration
+
+// string name
+var name = "nabin";
+console.log(name);
+
+// number 
+var age = 23;
+console.log(age);
+
+var _name = "nabin poudel";
+console.log(_name);
+
+var _name$ = "siwan";
+console.log(_name$);
+
+var $name = "siwan poudel"
+console.log($name);
+
+
+// invalid variable declaration
+
+// string must be written in quotation martk
+var name = nabin;
+console.log(name);
+
+// special symbol not used to define variable
+// var @name = "nabin";
+// console.log(@name);
+
+// number not used to define bvariable
+// var 12age = 23;
+// console.log(12age);
+
+// variable declaration is wrught but the value conver to string not use in mathematical operation 
+var age = "123";
+console.log(age);
