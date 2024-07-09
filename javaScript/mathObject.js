eeffc7fd56b238c32a136254782aad80aab79e47
Nabin Poudel <nabinpoudel788@gmail.com>
2024-07-09 21:14:19 +0545

    Math Object: Math.round.floor.ceil.PI

---
+++ javaScript/mathObject.js
@@ -0,0 +1,24 @@
+// ! Math Object in javaScript
+// ? in java Script there are various way to used math objetc.
+// * 1. math.round.
+// It will round figure the value and display the nearest single digit value
+console.log("Math.round");
+console.log(Math.round(4.4));
+console.log(Math.round(4.5));
+
+// * 2. Math.floor. display nearest down to integer
+// It will floor figure the value and display the current single digit value.
+console.log("Math.floor");
+console.log(Math.floor(4.4));
+console.log(Math.floor(4.5));
+
+// * 3. Math.ceil. display nearest up to integer.
+// It will ceil figure the value and display the upcomming single digit value.
+console.log("Math.ceil");
+console.log(Math.ceil(4.4));
+console.log(Math.ceil(4.5));
+
+// * It will display the PI object
+console.log("PI value using Math object:",Math.PI);
+
+
