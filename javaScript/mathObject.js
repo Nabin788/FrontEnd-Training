da030f25fe20870c6a3a86024db102c3d388f46e
Nabin Poudel <nabinpoudel788@gmail.com>
2024-07-09 21:44:18 +0545

    Exponential and logorithmic function

--- javaScript/mathObject.js
+++ javaScript/mathObject.js
@@ -18,7 +18,34 @@ console.log("Math.ceil");
 console.log(Math.ceil(4.4));
 console.log(Math.ceil(4.5));
 
-// * It will display the PI object
+// *4. It will display the PI object
 console.log("PI value using Math object:",Math.PI);
+
+// * 5. Math.trunc. It is added in 2015 ES6.It is same as .floor method but only difference is that it 
+// It will return the value of x by removing point value
+console.log(Math.trunc(3));
+console.log(Math.trunc(3.1));
+console.log(Math.trunc(-3));
+console.log("Math .floor in negtive scenario", Math.floor(-3.1));
+console.log("Math.trunc in negative scenario", Math.trunc(-3.1));
+
+
+// ? Exponsitial and logorithmic function
+//  *6 Math.pow. pow is an power. There arer two way to used power method
+// 2^3 = 2*2*2 = 8
+console.log(Math.pow(2,3));
+// ECMA define another way to used power 
+console.log(2 ** 3);
+
+//  *7 Math.sqrt 
+// it will find the squart of 25. 5*5 or 5^2 = 25
+console.log(Math.sqrt(25));
+
+// * 8. Math.log
+// we can used log methd to find the log value 
+console.log(Math.log(10));
+console.log(Math.log2(10));
+
+
 
 
