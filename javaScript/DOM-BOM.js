0ba4557778ecaae9214cfcd4e79f5934a1b15390
Nabin Poudel <nabinpoudel788@gmail.com>
2024-07-10 08:54:08 +0545

    navigating the document

--- javaScript/DOM-BOM.js
+++ javaScript/DOM-BOM.js
@@ -17,3 +17,32 @@
 // Processing node : <? xml-stylesheet ... ?>
 // Document node : A document node.
 // Document type node : <! Doctype html>
+
+// ! DOM Navigation
+// It will navigate the overal html code
+let parent = document;
+console.log(parent);
+// It will navigate only head tag element
+let documentHead = document.head;
+console.log(documentHead);
+// It will navigate only body tag element
+let documentBody = document.body;
+console.log(documentBody);
+// It will navigate tag used in html body.
+let bodyChildren = documentBody.children;
+console.log(bodyChildren);
+//  It will navigate the detailed tag element used in body
+let bodyChildrenNodes = documentBody.childNodes;
+console.log(bodyChildrenNodes);
+// It will navigate the first elemetn of body.
+let firstElementChild = documentBody.firstElementChild
+console.log(firstElementChild);
+// It will navigate the last Element of body
+let lastElementChild = documentBody.lastElementChild;
+console.log(lastElementChild);
+// It will navigate to next sibling of child
+let nextElementSibling = firstElementChild.nextElementSibling;
+console.log(nextElementSibling);
+// It will navigate to previous sibling of next child
+let previousElementSibling = nextElementSibling.previousElementSibling;
+console.log(previousElementSibling);
