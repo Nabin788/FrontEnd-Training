e9b4969d218657b3d4dbb92a650f251f943a9eaa
Nabin Poudel <nabinpoudel788@gmail.com>
2024-07-10 08:25:35 +0545

    DOM and BOM in JavaScript

---
+++ javaScript/DOM-BOM.js
@@ -0,0 +1,19 @@
+// ! Window object. Global window in a browser. Both the BOM and DOM are part of the window object.
+// ? 1. BOM 
+// * Browser Object Model provides the methods and properties for interacting with the browser itself. 
+// * It focuse on browser enviroment
+// BOM feature includes window.navigator for browser information
+// window.location for URL manipulation.
+// window.alert for displaying the alert.
+
+// ? 2. DOM is an API. Document Object Model. Create tree like structure
+// * modifying the the HTML code with the help of Js dynamically
+// * It focus on the content of the page.
+// * Type of nodes
+// Element node : Html tag, tree building blocks
+// text node : text content including new lines, spaces and tabs.
+// attribures node: attribute of an element.
+// comment node : Comment of HTML.
+// Processing node : <? xml-stylesheet ... ?>
+// Document node : A document node.
+// Document type node : <! Doctype html>
