59fb1953d107648188cf68f2950fafbe5c304307
Nabin Poudel <nabinpoudel788@gmail.com>
2024-07-18 16:56:14 +0545

    pass by value and pass by reference

--- javaScript/object.js
+++ javaScript/object.js
@@ -6,7 +6,7 @@ let objValue = {
     age: 24,
     address: "Bhaluwan-3, Dang",
     Gender: true,
-    computer: function(){
+    computer: function () {
         console.log("Welcome to my java Script learning season");
     }
 }
@@ -19,7 +19,7 @@ console.log(objValue.Gender);
 
 // * we can add key value on define object and also modify
 // Here we can add key value by using notatio or bracket
-objValue["married Status"] = "Unmarried"; 
+objValue["married Status"] = "Unmarried";
 // Here we can modify value by using notatio or bracket
 objValue.Gender = "Male";
 
@@ -33,8 +33,38 @@ let object = {
     [idType]: "019530115",
     Name: "Nabin Poudel",
     address: "Bhaluwang",
-    collegeFun: function(){
+    collegeFun: function () {
         console.log(`My ${idType} is ${object[idType]} and My Name is ${object.Name}`);
     }
 }
 object.collegeFun();
+
+// ! Pass by value and Pass By reference
+// ? pass By value
+// * In the given example we can see that we can share the value of num to other and the original value not affect.
+// By sharing the copy of value and not affect original state called pass by value or primitive
+let num = 10;
+let calculation = ((x) => {
+    return num + x;
+});
+console.log(calculation(num));
+console.log(num);
+
+// ? pass By Reference
+// * In the given example we can see that we can share the value of objReference to other and the original value affect.
+// By sharing the copy of value and affect the original state called pass by Reference
+let objReference = {
+    id: 1,
+    name: "siwan",
+    age: "20"
+}
+// In the given log the value will display original name define within an object
+console.log(objReference.name);
+// here the key value will be share to other variable
+let newName = objReference;
+// Here the share key value redefine to new value
+newName.name = "Siwan Poudel";
+// here the log will display the variable where the redefine value store 
+console.log(newName.name);
+// here the log display original key bvalue variable but the name value is changed because of pass by rederence
+console.log(objReference.name);
